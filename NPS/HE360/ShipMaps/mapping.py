d155c3f8baa5e06bc4a14d5288fd59e064ec6d9a
Rishi Wadhwa <rishi@wadhwas.us>
2023-07-10 13:59:05 -0700

    Comments added

--- NPS/HE360/ShipMaps/mapping.py
+++ NPS/HE360/ShipMaps/mapping.py
@@ -1,3 +1,4 @@
+# imports
 import pandas as pd
 from shapely.geometry import Point
 import geopandas as gpd
@@ -7,18 +8,22 @@ import os
 import csv
 import cartopy.crs as ccrs
 
+# all-data csv reading setup
 main_csv = open('../HE-CSVs/all-data.csv')
 csv_reader = csv.reader(main_csv)
 header = next(csv_reader)
 
+# comm channels/links csv reading setup
 comms_csv = open('../HE-CSVs/all-comms.csv')
 comm_reader = csv.reader(comms_csv)
 header = next(comm_reader)
 
+# Planet.com/HE360 matched csv reading setup
 matched_csv = open('../Planet/matched-data.csv')
 matched_reader = csv.reader(matched_csv)
 header = next(matched_reader)
 
+# getting ALL ids that exist in both platforms (HE360 & Planet.com)
 matched_ids = []
 for line in matched_reader:
     try:
@@ -26,18 +31,22 @@ for line in matched_reader:
     except:
         pass
 
+# getting all the COMM channels and names into an array (provided by HE360)
 comms = [line for line in comm_reader]
 for i in range(len(comms)):
     comms[i][0] = int(comms[i][0])
 
+# getting ALL data from HE360
 all_data = [line for line in csv_reader]
 
 all_coords = {}
 all_comms = {}
 
+# comm dict setup in format { "id": ["frequency(MHz)", "channel name"] }
 for commlink in comms:
-    all_comms[commlink[0]] = [commlink[1], commlink[2]] # [freq, channel name]
+    all_comms[commlink[0]] = [commlink[1], commlink[2]]
 
+# all coords dict setup in format { "id": ["x/long", "y/lat"] }
 for dp in all_data:
     if dp[0] not in all_coords.keys():
         all_coords[dp[0]] = []
@@ -45,6 +54,7 @@ for dp in all_data:
     ar.append([dp[1], dp[2]])
     all_coords[dp[0]] = ar
 
+# setup base map of the globe using Plate Carree maps
 def mapping_base(id):
     df = pd.read_csv("../HE-CSVs/all-data.csv", delimiter=',', skiprows=0, low_memory=False)
     geometry = [Point(xy) for xy in zip(df['x'], df['y'])]
@@ -56,13 +66,13 @@ def mapping_base(id):
     plt.ylim(-90,90)
     plt.scatter([-117.1300], [32.6848], color='yellow')
     plt.text(-117.1300, 32.6848, "SD Navy Base", color='yellow')
-    # plt.xlabel(f"{all_comms[int(ship_id)][1]}", color='blue')
-    # plt.ylabel(f"{all_comms[int(ship_id)][0]}", color='blue')
 
+# all matched ids indexes in all_coords
 planet_ids = []
 for a_id in matched_ids:
     keys = list(all_coords.keys())
     planet_ids.append(keys.index(str(a_id)) + 1)
+
 
 
 while True:
@@ -81,23 +91,27 @@ while True:
                 print('Invalid, try again.')
     if ans == 0:
         break
-    else:         
+    else:
+        # print SPECIFIC map defined by "ans" and "ship_id"         
         ship_id = list(all_coords.keys())[ans-1]
         mapping_base(ship_id)
         ans = 0
         big_ar = all_coords[ship_id]
+
+        # find point of ship and place on map
         x, y = [], []
         for ar in big_ar:
             x.append(float(ar[0]))
             y.append(float(ar[1]))
         plt.scatter(x,y, color='red', transform=ccrs.PlateCarree())
-        plt.text(x[0],y[0],f"Point!",color='red')
+        plt.text(x[0],y[0],f"Located Ship",color='red')
         freq = all_comms[int(ship_id)][0] if all_comms[int(ship_id)][0] != '' else '---'
         try:
             freq = float(freq) / 1000.0 # MHz to GHz
         except:
             pass
         
+        # basic ship info title
         plt.suptitle(f'Ship: {ship_id}\n{all_comms[int(ship_id)][1]} | {freq}GHz', fontsize=12)
         
         status = True
@@ -105,7 +119,7 @@ while True:
             status = False
 
         classification = 'Unknown'
-        # X-band, S-band, iridium, GPS, VHF, L-band
+        # X-band, S-band, iridium, GPS, VHF, L-band classification statement
         if all_comms[int(ship_id)][1] == 'X-Band RADAR':
             classification = 'Government/Military' # uncommon for civilian, commonly found in military/government applications
         elif all_comms[int(ship_id)][1] == 'S-Band RADAR':
@@ -121,8 +135,8 @@ while True:
         else:
             classification = 'Civilian'
 
+        # more ship information as subtitle
         planet_status = 'Yes' if status else 'No'
         plt.title(f'HE360: Yes  |  Planet.com: {planet_status}\nPlanet.com: {classification}', fontsize=8, loc='right')
-        print(matched_ids)
-        print(ship_id)
+        
         plt.show()
